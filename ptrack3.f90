d0196db0e12dd94fd2d627fa4c277ef278beaed5
Jiabi Du <jiabi.du@gmail.com>
2022-09-15 21:42:57 -0500

    minor edit

--- ptrack3.f90
+++ ptrack3.f90
@@ -130,7 +130,7 @@
       real, allocatable :: zlcl(:),real_ar(:,:)
       integer :: nodel2(3)
       integer :: varid1,varid2,dimids(3),istat,nvtx,iret
-      CHARACTER(LEN=200) :: ncFile
+      character(len=200) :: ncFile
       integer::prcount,NCID2,numparID,timeID,modtimeID,lonID,latID,depthID
       
 
